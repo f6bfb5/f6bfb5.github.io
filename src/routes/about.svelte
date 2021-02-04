cff3b9ef0366ca47d7e62c7473e9abeea4d89026
f6bfb5 <f6bfb5@gmail.com>
2021-02-05 02:36:19 +0800

    Doc: edit about page

--- src/routes/about.svelte
+++ src/routes/about.svelte
@@ -1,14 +1,24 @@
-<style>
-</style>
-
 <svelte:head>
   <title>About</title>
 </svelte:head>
 
 <div class="container">
   <h1>About</h1>
-  <p>
+  <img src="https://i.imgur.com/0hqnDr5.jpg" alt="cat" class="about-hero-img" />
+  <p class="about-hero-p">
     「願意聽別人說話的人並不多。每個人都只想說話，而說的話又不是什麼有內容的話。我也是其中之一。」
   </p>
   <p>「該說的話都已經被說過，但是因為沒人在聽，所以還得全都再說一遍。」</p>
 </div>
+
+<style>
+  .container {
+    overflow: hidden;
+  }
+
+  .about-hero-img {
+    float: right;
+    width: 40%;
+    margin-left: 1em;
+  }
+</style>
