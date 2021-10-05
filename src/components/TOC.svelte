680509729878f95f77a09391278121e4f1f12575
f6bfb5 <f6bfb5@gmail.com>
2021-10-06 00:17:12 +0800

    Style: change TOC to win98-like style

--- src/components/TOC.svelte
+++ src/components/TOC.svelte
@@ -21,10 +21,12 @@
   });
 </script>
 
-<div class="toc-dropdown">
-  {#if TOC.length !== 0}
-    <div class="toc">
-      <div class="toc--title">{title}</div>
+{#if TOC.length !== 0}
+  <div class="toc">
+    <div class="toc--title">
+      {title}
+    </div>
+    <div class="toc-dropdown">
       <ul>
         {#each TOC as t}
           <li>
@@ -41,26 +43,47 @@
         <ul />
       </ul>
     </div>
-    <div class="toc-menu" />
-  {/if}
-</div>
+    <!-- <div class="toc-menu" /> -->
+  </div>
+{/if}
 
 <style>
-  .toc-dropdown {
+  .toc {
     display: none;
   }
+  @media all and (min-width: 1280px) {
+    .toc {
+      left: calc((100vw - 720px) / 2 - 280px) !important;
+      max-width: 240px !important;
+    }
+  }
   @media all and (min-width: 1150px) {
-    .toc-dropdown {
-      display: block;
+    .toc {
+      display: flex;
+      flex-direction: column;
+
       position: fixed;
       top: 250px;
       left: calc((100vw - 720px) / 2 - 200px);
       max-width: 150px;
       max-height: calc(100vh - 300px);
-      overflow-y: scroll;
+
+      /* [98.css - A design system for building faithful recreations of old UIs](https://jdan.github.io/98.css/) */
+      background: silver;
+      box-shadow: inset -1px -1px #0a0a0a, inset 1px 1px #dfdfdf,
+        inset -2px -2px grey, inset 2px 2px #fff;
+      padding: 3px;
     }
     .toc--title {
+      background: linear-gradient(90deg, navy, #1084d0);
+      color: white;
+      padding: 3px 2px 3px 3px;
       font-size: 12px;
+      letter-spacing: 0px;
+    }
+    .toc-dropdown {
+      padding: 3px 2px 3px 3px;
+      overflow-y: scroll;
     }
     .toc ul {
       list-style-position: inside;
@@ -68,15 +91,26 @@
     }
     .toc ul li {
       padding: 0;
+      font-weight: 600;
     }
     .toc ul li::marker {
-      content: "# ";
+      content: "‧";
     }
     .toc ul li + li {
-      margin-top: 8px;
+      margin-top: 2px;
     }
     a {
       cursor: pointer;
     }
+  }
+
+  ::-webkit-scrollbar-thumb {
+    background-color: #dfdfdf;
+    box-shadow: inset -1px -1px #0a0a0a, inset 1px 1px #fff,
+      inset -2px -2px grey, inset 2px 2px #dfdfdf;
+  }
+
+  ::-webkit-scrollbar-corner {
+    background: #dfdfdf;
   }
 </style>
