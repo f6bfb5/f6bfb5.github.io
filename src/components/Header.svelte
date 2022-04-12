72efc43288837910a3239b1498dd1a91ad2c8a70
f6bfb5 <f6bfb5@gmail.com>
2022-04-13 07:30:10 +0800

    add progress bar in the bottom of header

--- src/components/Header.svelte
+++ src/components/Header.svelte
@@ -2,21 +2,23 @@
   import Logo from "./Logo.svelte";
   // import DarkmodeButton from "./DarkmodeButton.svelte";
   import Nav from "./Nav.svelte";
+  import ProgressBar from "./ProgressBar.svelte";
 
   export let segment;
 </script>
 
 <header>
-  <Nav {segment} />
-  <!-- <DarkmodeButton /> -->
-  <Logo />
+  <div class="row">
+    <Nav {segment} />
+    <!-- <DarkmodeButton /> -->
+    <Logo />
+  </div>
+  <ProgressBar />
 </header>
 
 <style>
   header {
-    display: flex;
     margin: 0 auto;
-    padding: 2em 0 1em 0;
     max-width: 720px;
     width: 100%;
 
@@ -25,5 +27,9 @@
     z-index: 10;
 
     background-color: var(--bg-color);
+  }
+  .row {
+    display: flex;
+    padding: 2em 0 1em 0;
   }
 </style>
---
+++ src/components/ProgressBar.svelte
@@ -0,0 +1,28 @@
+<!-- [How to Build a Reading Progress Bar With CSS and JavaScript](https://webdesign.tutsplus.com/tutorials/reading-progress-bar-css-javascript--cms-36635) -->
+<script>
+  let scrollPercent = "0%";
+  let processScroll = () => {
+    let docElem = document.documentElement,
+      docBody = document.body,
+      scrollTop = docElem["scrollTop"] || docBody["scrollTop"],
+      scrollBottom =
+        (docElem["scrollHeight"] || docBody["scrollHeight"]) -
+        window.innerHeight;
+    scrollPercent = (scrollTop / scrollBottom) * 100 + "%";
+  };
+</script>
+
+<svelte:window on:scroll={processScroll} />
+<div id="progress-bar" style="--scrollAmount: {scrollPercent};" />
+
+<style>
+  #progress-bar {
+    background-image: linear-gradient(
+      120deg,
+      var(--subtitle-color) 0%,
+      var(--subtext-color) 100%
+    );
+    width: var(--scrollAmount);
+    height: 2px;
+  }
+</style>
