62a6d15e88a6752cb7d7ed3e295606bd8a46c297
f6bfb5 <f6bfb5@gmail.com>
2022-05-02 22:31:33 +0800

    update about page

--- src/routes/about.svelte
+++ src/routes/about.svelte
@@ -1,91 +1,143 @@
+<script>
+  const backgroundImageUrls = [
+    "https://media4.giphy.com/media/PZrjGkr334fXa/giphy.gif",
+    "https://media0.giphy.com/media/udK21RQeWtaGQ/giphy.gif",
+    "https://c.tenor.com/xK6H41lmjrQAAAAC/anime-girl.gif",
+    "https://c.tenor.com/ZwiXDI5sKe0AAAAC/lain-serial-experiments-lain.gif",
+    "https://media4.giphy.com/media/nlk3Y1u3r1n5C/giphy.gif",
+    "https://media0.giphy.com/media/jsoMtBuP1Ahpu/giphy.gif",
+    "https://c.tenor.com/0lv-4Uu_h_EAAAAC/giygas.gif",
+    "https://c.tenor.com/Ky-q-qGYd4AAAAAd/darker-than-black-dtb-hei.gif",
+    "https://c.tenor.com/8LexFYcsTmQAAAAd/va11hall-a-jill.gif",
+    "https://c.tenor.com/SjuAbL0uMakAAAAd/yume-nikki-yume.gif",
+    "https://i.imgur.com/P3EwTea.gif",
+    "https://c.tenor.com/TCEyVCo9wG0AAAAC/dark-souls-bonfire.gif",
+    "https://c.tenor.com/URM4MiNuWrUAAAAd/pixel-art-shrine.gif",
+    "https://c.tenor.com/BrNtIejIcXUAAAAC/pixel-art.gif",
+    "https://c.tenor.com/a6pDrWf6gJgAAAAd/pixel-city-art.gif",
+    "https://c.tenor.com/cYRncpJTFXcAAAAd/waneella-pixel-art.gif",
+    "https://i.imgur.com/SqCn8Gj.jpg",
+  ];
+  const backgroundImage =
+    backgroundImageUrls[Math.floor(backgroundImageUrls.length * Math.random())];
+
+  const text = "追体験で人は幸福になれない";
+</script>
+
 <svelte:head>
   <title>About</title>
 </svelte:head>
 
-<div class="container">
-  <h1>About</h1>
-  <figure>
-    <img src="https://i.imgur.com/pA9yPzD.jpg" alt="television-man" />
-    <figcaption>
-      <a href="https://unsplash.com/photos/KssYQvWXCv0" targer="blank">
-        Yondon Batbileg(@prike) - Unsplash
-      </a>
-    </figcaption>
-  </figure>
-  <div class="bubble-container">
-    <img
-      src="./e0724_0.svg"
-      onerror="this.onerror=null; this.src=''"
-      alt="Message Bubble"
-      class="bubble-border"
-    />
-    <div class="bubble-content">
-      <p>
-        「願意聽別人說話的人並不多。每個人都只想說話，而說的話又不是什麼有內容的話。我也是其中之一。」
-      </p>
-      <p>「該說的話都已經被說過，但是因為沒人在聽，所以還得全都再說一遍。」</p>
-    </div>
-  </div>
-  <div class="clearfix" />
-  <hr />
-  <p style="font-size: 0.875rem; text-align: center;">
-    いつでも捜しているよ
-    <br />どっかに君の姿を
-    <br />カバンの中も 机の中も
-    <br />探したけれど見つからないのに
-    <br />それより僕と踊りませんか
-    <br />朝が来るまで終わる事のないダンスを
-    <br />踊ってない夜を知らない
-    <br />踊ってない夜が気に入らない
-    <br />君は知ってるかい？
-    <br />踊らな死ぬ事を
-  </p>
-  <p style="font-size: 0.875rem; text-align: center;">
-    Make me sad, Make me mad, Make me feel alright.
-  </p>
+<div
+  class="about--background"
+  style="background-image: url('{backgroundImage}')"
+>
+  <a
+    href="https://www.youtube.com/watch?v=8ftDjebw8aA"
+    class="about--background-caption vhs"
+    target="_blank"
+    data-text={text}
+  >
+    {text}
+  </a>
 </div>
 
-<style>
-  .container {
+<style scoped>
+  .about--background {
+    position: fixed;
+    top: 50%;
+    left: 50%;
+    min-width: 100%;
+    min-height: 100%;
+    width: auto;
+    height: auto;
+    z-index: 1000;
+    transform: translate(-50%, -50%);
     overflow: hidden;
-    padding: 0 16px 16px;
+
+    background-color: black;
+    /* background-image: url('https://i.imgur.com/pA9yPzD.jpg'); */
+    background-size: cover;
+    background-position: center;
+    background-attachment: fixed;
+
+    display: flex;
+    justify-content: center;
+    align-items: center;
+
+    color: white;
   }
 
-  figure {
-    float: right;
-    width: 40%;
-    margin-left: 1em;
-    margin-bottom: 1em;
+  .about--background-caption {
+    align-self: center;
+    min-height: 100%;
+
+    font-size: 40px;
+    text-align: center;
   }
 
-  .clearfix {
-    clear: both;
+  /* .hide {
+    background-color: var(--title-color);
+    color: var(--title-color);
   }
+  .hide:hover {
+    color: var(--subtitle-color);
+  } */
 
-  .bubble-container {
+  /* https://codepen.io/simonbreiter/pen/NrmyjR */
+  .vhs {
+    text-transform: uppercase;
     position: relative;
-    width: 70%;
+    mix-blend-mode: screen;
+    color: transparent;
+    text-shadow: 0 0 0.125rem #0f0;
+    animation: jitter 5s infinite;
   }
-  .bubble-border {
+  .vhs::before,
+  .vhs::after {
+    content: attr(data-text);
     position: absolute;
+    top: 0;
+    left: 0;
+    mix-blend-mode: screen;
+    animation: glitch 5s infinite alternate;
+  }
+  .vhs::before {
+    text-shadow: 0 0 0.125rem #f00;
+  }
+  .vhs::after {
+    text-shadow: 0 0 0.125rem #00f;
+    -webkit-animation-delay: 5s;
+    animation-delay: 5s;
   }
 
-  .bubble-content {
-    position: absolute;
-    padding: 7.5% 12.5% 0 15%;
-    color: #171717;
-  }
-  .bubble-content p {
-    padding: 10px 0;
-  }
-  @media screen and (max-width: 720px) {
-    .bubble-content {
-      padding: 7.5% 12.5% 0 20%;
+  @keyframes glitch {
+    0% {
+      transform: rotate(0deg) translate(0.11rem) rotate(0deg);
     }
-    .bubble-content p {
-      padding: 0 0 1.38vw 0;
-      font-size: 2.5vw;
-      line-height: calc(0.5vw * 7);
+    100% {
+      transform: rotate(360deg) translate(0.11rem) rotate(-360deg);
+    }
+  }
+
+  @keyframes jitter {
+    0% {
+      transform: translateX(0);
+    }
+    50% {
+      transform: translateX(0);
+    }
+    50.1% {
+      transform: translateX(0.1em) skew(40deg);
+    }
+    50.2% {
+      transform: translateX(-0.2em) skew(-40deg);
+    }
+    50.3% {
+      transform: translateX(0);
+    }
+    100% {
+      transform: translateX(0);
     }
   }
 </style>
