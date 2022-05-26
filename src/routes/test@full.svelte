af24437f753b4d5421dc2ae39cebf3c47ffc0f90
f6bfb5 <f6bfb5@gmail.com>
2022-05-27 00:47:55 +0800

    change font

--- src/routes/test@full.svelte
+++ src/routes/test@full.svelte
@@ -100,7 +100,7 @@
   <link rel="preconnect" href="https://fonts.googleapis.com" />
   <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
   <link
-    href="https://fonts.googleapis.com/css2?family=DM+Serif+Display&display=swap"
+    href="https://fonts.googleapis.com/css2?family=Zen+Old+Mincho:wght@900&display=swap"
     rel="stylesheet"
   />
 </svelte:head>
@@ -190,7 +190,7 @@
     justify-content: center;
     align-items: center;
 
-    font-family: "DM Serif Display", serif;
+    font-family: "Zen Old Mincho", serif;
     color: white;
     background-color: black;
 
