6a3dad3275b0fbf8d6b6c9cb9f7e447a49c795ad
f6bfb5 <f6bfb5@gmail.com>
2022-05-27 19:20:52 +0800

    update test page

--- src/routes/test@full.svelte
+++ src/routes/test@full.svelte
@@ -3,9 +3,98 @@
 
   let text1 = "",
     text2 = "";
+  let color1 = "white",
+    color2 = "red";
   let sentences = [
-    ["ﾈｺ", "と和解せよ"],
-    ["", ""],
+    ["失望", "の海", "red", "white"],
+    ["", "", "white", "white"],
+    ["脆弱な", "心", "white", "red"],
+    ["", "", "white", "white"],
+    ["偽り", "の微笑み", "red", "white"],
+    ["", "", "white", "white"],
+    ["病的な", "被写体", "white", "red"],
+    ["", "", "white", "white"],
+    ["自我の", "崩壊", "white", "red"],
+    ["", "", "white", "white"],
+    ["残酷", "な他人", "red", "white"],
+    ["", "", "white", "white"],
+    ["代理の", "異性", "white", "red"],
+    ["", "", "white", "white"],
+    ["刹那", "な癒し", "red", "white"],
+    ["", "", "white", "white"],
+    ["蔓延する", "虚脱", "white", "red"],
+    ["", "", "white", "white"],
+    ["無", "への願望", "red", "white"],
+    ["", "", "white", "white"],
+    ["閉塞", "した自分", "red", "white"],
+    ["", "", "white", "white"],
+    ["分離への", "不安", "white", "red"],
+    ["", "", "white", "white"],
+    ["一方的な", "勘違い", "white", "red"],
+    ["", "", "white", "white"],
+    ["他人の", "恐怖", "white", "red"],
+    ["", "", "white", "white"],
+    ["危険", "な思考", "red", "white"],
+    ["", "", "white", "white"],
+    ["他者との", "拒絶", "white", "red"],
+    ["", "", "white", "white"],
+    ["同調への", "嫌悪", "white", "red"],
+    ["", "", "white", "white"],
+    ["傲慢", "な把握", "red", "white"],
+    ["", "", "white", "white"],
+    ["弱者への", "憐れみ", "white", "red"],
+    ["", "", "white", "white"],
+    ["不愉快", "な写真", "red", "white"],
+    ["", "", "white", "white"],
+    ["過去の", "傷痕", "white", "red"],
+    ["", "", "white", "white"],
+    ["曖昧", "な境界", "red", "white"],
+    ["", "", "white", "white"],
+    ["常識の", "逸脱", "white", "red"],
+    ["", "", "white", "white"],
+    ["孤独な", "ヒトビト", "red", "white"],
+    ["", "", "white", "white"],
+    ["価値への", "疑問", "white", "red"],
+    ["", "", "white", "white"],
+    ["情欲", "との融合", "red", "white"],
+    ["", "", "white", "white"],
+    ["胎内", "への回帰", "red", "white"],
+    ["", "", "white", "white"],
+    ["空しい", "時間", "red", "white"],
+    ["", "", "white", "white"],
+    ["破滅", "への憧憬", "red", "white"],
+    ["", "", "white", "white"],
+    ["要らない", "ワタシ", "white", "red"],
+    ["", "", "white", "white"],
+    ["", "", "white", "white"],
+    ["", "", "white", "white"],
+    ["", "", "white", "white"],
+    ["", "", "white", "white"],
+    ["虚構", "の始まり", "red", "white"],
+    ["虚構", "の始まり", "red", "white"],
+    ["", "", "white", "white"],
+    ["", "", "white", "white"],
+    ["現実の", "続き", "white", "red"],
+    ["現実の", "続き", "white", "red"],
+    ["", "", "white", "white"],
+    ["", "", "white", "white"],
+    ["", "", "white", "white"],
+    ["", "", "white", "white"],
+    ["それは、", "夢の終わり", "white", "red"],
+    ["それは、", "夢の終わり", "white", "red"],
+    ["それは、", "夢の終わり", "white", "red"],
+    ["それは、", "夢の終わり", "white", "red"],
+    ["", "", "white", "white"],
+    ["", "", "white", "white"],
+    ["", "", "white", "white"],
+    ["", "", "white", "white"],
+    ["", "", "white", "white"],
+    ["", "", "white", "white"],
+    ["", "", "white", "white"],
+    ["", "", "white", "white"],
+    ["", "", "white", "white"],
+    ["", "", "white", "white"],
+    ["", "", "white", "white"],
   ];
   let index = 0;
 
@@ -86,6 +175,8 @@
     interval = setInterval(() => {
       text1 = sentences[index][0];
       text2 = sentences[index][1];
+      color1 = sentences[index][2];
+      color2 = sentences[index][3];
       index++;
       if (index >= sentences.length) index = 0;
     }, 167);
@@ -113,9 +204,12 @@
     height="1400"
     style="opacity: 0.05;"
   />
+  <div class="ht">では、<br />あなたは何故、ココにいるの？</div>
   <div class="tc">
-    <span class="t vhs" style="color: red;" data-text={text1}>{text1}</span>
-    <span class="t vhs" data-text={text2}>{text2}</span>
+    <span class="t vhs" style="color: {color1};" data-text={text1}>{text1}</span
+    >
+    <span class="t vhs" style="color: {color2};" data-text={text2}>{text2}</span
+    >
   </div>
 </div>
 
@@ -181,7 +275,13 @@
   </defs>
 </svg>
 
+
+
 <style scoped>
+  :global(:root) {
+    --outer-bg-color: #460609;
+  }
+
   .c {
     width: 100%;
     min-height: 100vh;
@@ -190,26 +290,55 @@
     justify-content: center;
     align-items: center;
 
+    position: relative;
+
     font-family: "Zen Old Mincho", serif;
     color: white;
-    background-color: black;
+
+    background: linear-gradient(
+        217deg,
+        rgba(0, 0, 0, 0.8),
+        rgba(255, 0, 0, 0) 70.71%
+      ),
+      linear-gradient(127deg, rgba(0, 0, 0, 0.8), rgba(255, 0, 0, 0) 70.71%),
+      linear-gradient(336deg, rgba(0, 0, 0, 0.8), rgba(255, 0, 0, 0) 70.71%);
 
     user-select: none;
     overflow: hidden;
   }
   .tc {
-    padding: 1em;
+    padding: .5em;
     text-align: center;
+
+    z-index: 10;
   }
   .t {
     display: inline-block;
     width: fit-content;
     height: fit-content;
 
-    font-size: 48px;
+    font-size: 5.5em;
     white-space: pre-line;
+    text-align: center;
 
     animation: jitter 5s infinite, squiggly-anim 0.34s linear infinite;
+  }
+
+  .ht {
+    padding: 1em;
+    position: absolute;
+    /* top: -50%; */
+    /* left: -50%; */
+
+    font-size: 3em;
+
+    transform: skewX(-30deg) rotateZ(-30deg);
+    animation: squiggly-anim 2s linear infinite;
+
+    text-align: center;
+    line-height: 3;
+    opacity: 0.2;
+    z-index: 10;
   }
 
   .vhs {
