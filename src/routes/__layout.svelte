79975578e4543c3f821319600a6f8a8fa7690fd2
f6bfb5 <f6bfb5@gmail.com>
2022-05-29 10:05:01 +0800

    add loader and make style and font preloading

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -4,6 +4,12 @@
   import Favicon from "$lib/Favicon.svelte";
 
   import { onMount } from "svelte";
+
+  let isLoaded = false;
+  function loaded() {
+    isLoaded = true;
+  }
+
   onMount(() => {
     window.dataLayer = window.dataLayer || [];
     function gtag() {
@@ -24,40 +30,98 @@
     src="https://www.googletagmanager.com/gtag/js?id=G-CRDP7WTXSQ"></script>
 </svelte:head>
 
-<div class="layout">
-  <!-- <GoogleAdsense id={ga_client_id} /> -->
+<svelte:window on:load={loaded()} />
 
-  <Favicon />
-  <Header />
+{#if !isLoaded}
+  <div class="layout">
+    <div class="loader-container">
+      <div class="loader">
+        <span class="loader-text loader-step1">/</span>
+        <span class="loader-text loader-step1">-</span>
+        <span class="loader-text loader-step1">\</span>
+        <span class="loader-text loader-step1">|</span>
+      </div>
+    </div>
+  </div>
+{:else}
+  <div class="layout">
+    <!-- <GoogleAdsense id={ga_client_id} /> -->
 
-  <main>
-    <slot />
-  </main>
+    <Favicon />
+    <Header />
 
-  <Footer />
-</div>
+    <main>
+      <slot />
+    </main>
+
+    <Footer />
+  </div>
+{/if}
 
 <style>
   .layout {
+    position: relative;
+
     display: flex;
     flex-direction: column;
-
-    position: relative;
 
     min-height: 100%;
     min-height: 100vh;
     background-color: var(--outer-bg-color);
   }
 
+  :root {
+    --loader-size: 24px;
+  }
+  .loader-container {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+
+    width: 100%;
+    min-height: 100%;
+    min-height: 100vh;
+    
+    background-color: black;
+    color: white;
+  }
+  .loader {
+    display: flex;
+
+    width: var(--loader-size);
+    white-space: nowrap;
+    overflow: hidden;
+    font-family: monospace;
+    
+    animation: loading-animation 0.5s steps(4, end) infinite;
+  }
+  .loader-text {
+    flex: 1;
+    min-width: var(--loader-size);
+    font-size: var(--loader-size);
+    text-align: center;
+  }
+  @keyframes loading-animation {
+    from {
+      text-indent: 0;
+    }
+    to {
+      text-indent: calc(var(--loader-size) * -8);
+    }
+  }
+
   main {
     position: relative;
+
     display: flex;
     flex: 1;
     flex-direction: column;
+
     margin: 0 auto;
     max-width: 720px;
     width: 100%;
     box-sizing: border-box;
+
     background-color: var(--bg-color);
   }
 </style>
