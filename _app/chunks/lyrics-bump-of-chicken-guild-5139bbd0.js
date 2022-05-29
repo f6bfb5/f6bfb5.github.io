8c594684f26b7808746ec4a0bac15ae1e80562fb
f6bfb5 <f6bfb5@gmail.com>
2022-05-29 08:10:49 +0000

    Deploying to gh-pages from  @ fd2f314ae59bdca0cde696d1a44348cc629f347e 🚀

---
+++ _app/chunks/lyrics-bump-of-chicken-guild-5139bbd0.js
@@ -0,0 +1,45 @@
+import{S as q,i as L,s as P,C as y,e as p,k as R,w as C,M as I,c as h,a as v,d as o,m as $,x as j,G as w,b as i,F as x,g as m,y as F,z as H,A as T,q as O,o as k,B as z,v as B,R as S,L as J,E as A}from"./index-26d5f122.js";import{P as G}from"./_post-0413a406.js";const{document:g}=J;function N(d){let e,_,s,l,c=`<code class="language-undefined">\u5F97\u5230\u4EBA\u985E\u9019\u4EFD\u5DE5\u4F5C \u5DF2\u7D93\u904E\u4E86\u591A\u4E45\u5462
+\u89BA\u5F97\u5B8C\u5168\u6C92\u6709\u62FF\u5230 \u6070\u5982\u5176\u5206\u7684\u85AA\u6C34
+
+\u4E0D\u77E5\u4F55\u6642\u8D77\u7684\u8AA4\u89E3 \u6211\u77E5\u9053\u300C\u9019\u4E0D\u662F\u5DE5\u4F5C\u300D
+\u7136\u800C\u4F3C\u4E4E\u70BA\u6642\u5DF2\u665A \u5DF2\u7D93\u8B8A\u6210\u53EA\u662F\u5DE5\u4F5C
+
+\u4E26\u975E\u89BA\u5F97\u60B2\u50B7 \u53EA\u662F\u5F88\u75B2\u618A
+\u8ACB\u8B93\u6211\u4F11\u606F \u6253\u7B97\u548C\u8AB0\u8AAA\u5462
+
+\u88AB\u596A\u8D70\u4EC0\u9EBC \u596A\u5F97\u4E86\u4EC0\u9EBC
+\u4E0D\u65B7\u5730\u91CD\u8986 \u9010\u6F38\u5730\u907A\u5FD8
+\u88AB\u6C61\u67D3\u7684 \u662F\u4E16\u754C\u9084\u662F\u81EA\u5DF1
+\u7121\u8AD6\u5982\u4F55 \u8A72\u5C07\u773C\u775B\u775C\u958B\u4E86
+\u9019\u5168\u90E8\u90FD\u662F\u4EE4\u4EBA\u760B\u72C2\u7684 \u6B63\u5E38\u7684\u65E5\u5E38\u751F\u6D3B
+
+\u611F\u5230\u9951\u9913\u7684\u7A7A\u6BBC \u4E0D\u9858\u52D5\u5F48 \u7B49\u5F85\u990C\u98DF
+\u6709\u8AB0\u80FD\u642D\u7406\u6211\u55CE \u6C92\u8AAA\u51FA\u53E3 \u53EA\u662F\u60F3\u8457
+
+\u4EBA\u985E\u9019\u982D\u8DEF\u88AB\u958B\u9664 \u5DF2\u7D93\u904E\u4E86\u591A\u4E45\u5462
+\u7E3D\u4E4B\u9084\u662F\u63EE\u7051\u6C57\u6C34 \u8A66\u8457\u52AA\u529B\u904E\u4E0B\u53BB
+
+\u6211\u56DE\u60F3\u8D77\u4E86\u90A3 \u8A31\u8A31\u591A\u591A\u7684\u4E8B
+\u6211\u80FD\u5920\u53BB\u9762\u5C0D \u6EFF\u904D\u7684\u7729\u76EE\u55CE
+
+\u5373\u4F7F\u6211\u4E0D\u7F8E\u9E97 \u7121\u6CD5\u6EAB\u67D4\u5F85\u4EBA
+\u6211\u9084\u80FD\u88AB\u5141\u8A31 \u7E7C\u7E8C\u547C\u5438\u8457\u55CE
+\u7B11\u8457\u61C9\u4ED8\u904E\u53BB \u5728\u93E1\u5B50\u524D\u54ED\u6CE3
+\u4E0D\u6703\u88AB\u6CE8\u610F\u5230 \u90A3\u662F\u7406\u6240\u7576\u7136 \u56E0\u70BA\u6211\u96B1\u85CF\u8457
+\u53EA\u662F\u904E\u8457\u540C\u6A23\u591C\u665A\u767D\u665D \u6B63\u5E38\u7684\u65E5\u5E38\u751F\u6D3B
+
+\u60F3\u88AB\u4EBA\u611B\u800C\u543C\u53EB\u8457 \u537B\u5BB3\u6015\u88AB\u4EBA\u6240\u611B
+\u800C\u9003\u9032\u5176\u4E2D\u7684\u7262\u7C60 \u6211\u8981\u5F9E\u9699\u4E2D\u62C9\u51FA
+\u5373\u4F7F\u6C61\u7A62\u4E5F\u63A5\u53D7\u5427 \u4E16\u754C\u662F\u6211\u81EA\u5DF1\u7684
+\u6C92\u6709\u95DC\u4FC2 \u4F60\u61C9\u8A72\u8981\u7528\u9019\u6A23\u7684\u59FF\u614B\u6D3B\u4E0B\u53BB
+\u9019\u5168\u90E8\u90FD\u662F\u4EE4\u4EBA\u760B\u72C2\u7684 \u6B63\u5E38\u7684\u65E5\u5E38\u751F\u6D3B
+
+\u5F97\u5230\u4E4B\u5F8C \u88AB\u958B\u9664\u5F8C
+\u904E\u4E86\u591A\u4E45 \u5728\u505A\u4EC0\u9EBC
+\u9019\u662F\u6211\u671F\u671B\u7684 \u9019\u662F\u6211\u6240\u9078\u7684
+\u300C\u9019\u4E0D\u662F\u5DE5\u4F5C\u300D \u6211\u77E5\u9053\u7684
+
+\u5F97\u5230\u4E4B\u5F8C \u88AB\u958B\u9664\u5F8C
+\u904E\u4E86\u591A\u4E45 \u5728\u505A\u4EC0\u9EBC
+\u9019\u662F\u6211\u671F\u671B\u7684 \u9019\u662F\u6211\u6240\u9078\u7684
+\u300C\u9019\u4E0D\u662F\u5DE5\u4F5C\u300D \u6211\u77E5\u9053\u7684</code>`;return{c(){e=p("iframe"),s=R(),l=p("pre"),this.h()},l(t){e=h(t,"IFRAME",{id:!0,src:!0,title:!0,frameborder:!0,allow:!0}),v(e).forEach(o),s=$(t),l=h(t,"PRE",{class:!0});var r=v(l);r.forEach(o),this.h()},h(){i(e,"id","video"),w(e.src,_="https://www.youtube.com/embed/wwVlJfJgWJI")||i(e,"src",_),i(e,"title","YouTube video player"),i(e,"frameborder","0"),i(e,"allow","accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"),e.allowFullscreen=!0,i(l,"class","language-undefined")},m(t,r){m(t,e,r),m(t,s,r),m(t,l,r),l.innerHTML=c},p:A,d(t){t&&o(e),t&&o(s),t&&o(l)}}}function X(d){let e,_,s,l,c,t,r;const u=[d[0],E];let b={$$slots:{default:[N]},$$scope:{ctx:d}};for(let a=0;a<u.length;a+=1)b=y(b,u[a]);return t=new G({props:b}),{c(){e=p("script"),s=p("script"),c=R(),C(t.$$.fragment),this.h()},l(a){const n=I('[data-svelte="svelte-7wz15u"]',g.head);e=h(n,"SCRIPT",{src:!0});var f=v(e);f.forEach(o),s=h(n,"SCRIPT",{src:!0});var M=v(s);M.forEach(o),n.forEach(o),c=$(a),j(t.$$.fragment,a),this.h()},h(){w(e.src,_="../subtitle/youtube.external.subtitle.min.js")||i(e,"src",_),w(s.src,l="../subtitle/subtitles.parser.js")||i(s,"src",l)},m(a,n){x(g.head,e),x(g.head,s),m(a,c,n),F(t,a,n),r=!0},p(a,[n]){const f=n&1?H(u,[n&1&&T(a[0]),n&0&&T(E)]):{};n&2&&(f.$$scope={dirty:n,ctx:a}),t.$set(f)},i(a){r||(O(t.$$.fragment,a),r=!0)},o(a){k(t.$$.fragment,a),r=!1},d(a){o(e),o(s),a&&o(c),z(t,a)}}}const E={title:"\u3010\u6B4C\u8A5E\u7FFB\u8B6F\u3011BUMP OF CHICKEN - \u30AE\u30EB\u30C9",date:"2018-12-24T23:09:12.000Z",tags:"Lyrics, Translate"};let Y="../subtitle/bump-of-chicken-guild.srt";function D(d,e,_){return B(()=>{var s=function(l,c){var t=new XMLHttpRequest;t.onreadystatechange=function(){if(t.readyState===XMLHttpRequest.DONE){var r=parser.fromSrt(t.responseText,!0);for(var u in r)r[u]={start:r[u].startTime/1e3,end:r[u].endTime/1e3,text:r[u].text};c(r)}},t.open("GET",l,!0),t.send(null)};s(Y,function(l){new YoutubeExternalSubtitle.Subtitle(document.getElementById("video"),l)})}),d.$$set=s=>{_(0,e=y(y({},e),S(s)))},e=S(e),[e]}class K extends q{constructor(e){super(),L(this,e,D,X,P,{})}}var W=Object.freeze(Object.defineProperty({__proto__:null,default:K,metadata:E},Symbol.toStringTag,{value:"Module"}));export{K as L,W as _,E as m};
