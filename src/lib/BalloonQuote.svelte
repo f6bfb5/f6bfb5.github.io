4825873ebc2a58fc6522f59a5b203b80e4621981
f6bfb5 <f6bfb5@gmail.com>
2022-08-02 14:47:44 +0100

    Add quote balloon and circle bordered component

---
+++ src/lib/BalloonQuote.svelte
@@ -0,0 +1,66 @@
+<script>
+  export let quote;
+  export let right;
+</script>
+
+<div class="balloon-quote {right ? 'right' : ''}">
+  <p>{quote}</p>
+</div>
+
+<style>
+  :root {
+    --balloon-quote-border-color: #555;
+    --balloon-quote-background-color: white;
+  }
+  .balloon-quote {
+    position: relative;
+    display: inline-block;
+    margin-left: 14px;
+    padding: 7px 10px;
+    min-width: 120px;
+    max-width: 100%;
+    border: 3px solid var(--balloon-quote-border-color);
+    background: var(--balloon-quote-background-color);
+    border-radius: 10px;
+  }
+  .balloon-quote:before {
+    content: "";
+    position: absolute;
+    top: 50%;
+    left: -24px;
+    margin-top: -12px;
+    border: 12px solid transparent;
+    border-right: 12px solid var(--balloon-quote-background-color);
+    z-index: 2;
+  }
+  .balloon-quote:after {
+    content: "";
+    position: absolute;
+    top: 50%;
+    left: -30px;
+    margin-top: -14px;
+    border: 14px solid transparent;
+    border-right: 14px solid var(--balloon-quote-border-color);
+    z-index: 1;
+  }
+  .balloon-quote.right {
+    margin-left: 0;
+    margin-right: 14px;
+  }
+  .balloon-quote.right:before {
+    left: auto;
+    border-right: none;
+    right: -12px;
+    border-left: 12px solid var(--balloon-quote-background-color);
+  }
+  .balloon-quote.right:after {
+    left: auto;
+    border-right: none;
+    right: -16px;
+    border-left: 14px solid var(--balloon-quote-border-color);
+  }
+  .balloon-quote p {
+    margin: 0;
+    padding: 0;
+  }
+</style>
---
+++ src/lib/CircleBorder.svelte
@@ -0,0 +1,43 @@
+<script>
+  export let text;
+  export let image;
+</script>
+
+<div class="circle-border">
+  {#if text}
+    <span class="circle-border-content">{text}</span>
+  {:else}
+    <img
+      class="circle-border-content image"
+      src={image}
+      alt="circle-bordered"
+    />
+  {/if}
+</div>
+
+<style>
+  .circle-border {
+    position: relative;
+    display: inline-block;
+    width: 80px;
+    height: 80px;
+    border-radius: 50%;
+    background: white;
+    border: 3px solid skyblue;
+    overflow: hidden;
+  }
+  .circle-border-content {
+    position: absolute;
+    display: inline-block;
+    width: 80px;
+    left: -3px;
+    top: 50%;
+    transform: translateY(-50%);
+    text-align: center;
+    object-fit: cover;
+  }
+  .image {
+    left: 0;
+    height: 80px;
+  }
+</style>
