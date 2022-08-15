337e482be3f8c8358ef638600d18199fc48b4e91
f6bfb5 <f6bfb5@gmail.com>
2022-08-15 17:31:37 +0100

    update components

--- src/lib/AutomaticColorText.svelte
+++ src/lib/AutomaticColorText.svelte
@@ -2,12 +2,22 @@
   // CSSのfilterを使って、背景色に合わせた文字色を自動的に設定する
   // https://blanktar.jp/blog/2020/11/css-automate-foreground-text-color
   export let backgroundColor;
+  export let size = "1em";
   export let text;
 </script>
 
-<span style="background-color: {backgroundColor};">
+<span
+  style="
+  display: inline-block; 
+  background-color: {backgroundColor};
+  "
+>
   <span
-    style="color: {backgroundColor}; filter: invert(100%) grayscale(100%) contrast(100);"
+    style="
+    color: {backgroundColor}; 
+    filter: invert(100%) grayscale(100%) contrast(100); 
+    font-size: {size};
+    "
   >
     {text}
   </span>
