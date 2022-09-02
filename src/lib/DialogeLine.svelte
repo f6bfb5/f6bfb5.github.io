65233de1d5aed2f1ae53e1283fe539e885a89ea3
f6bfb5 <f6bfb5@gmail.com>
2022-09-02 14:11:15 +0100

    update

---
+++ src/lib/DialogeLine.svelte
@@ -0,0 +1,13 @@
+<script>
+  export let speakerText;
+  export let speakerColor;
+  export let speakerBackground;
+</script>
+
+<p>
+  <span
+    style="padding: 2px; color: {speakerColor}; background: {speakerBackground};"
+  >
+    {speakerText}</span
+  >：<slot />
+</p>
